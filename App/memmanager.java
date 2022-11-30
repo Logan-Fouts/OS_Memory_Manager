cbe5ffca491f454e6d3f48981c195504f20dd514
Logan Fouts <112691772+Logan-Fouts@users.noreply.github.com>
2022-11-30 13:18:43 +0100

    Delete App directory

--- App/memmanager.java
+++
@@ -1,9 +0,0 @@
-package OS_Memory_Manager.App;
-
-import java.util.HashMap;
-
-public class memmanager {
-    public void startMemManager(HashMap<Integer, String> commands, inout readwrite) {
-        System.out.println(commands);
-    }
-}
